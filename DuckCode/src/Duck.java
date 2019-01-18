ba444c90df7303e12b9d669bc6b5d3866539e270
Michael Vance <mexvance04@gmail.com>
2019-01-18 09:43:33 -0700

    This is a new update

---
+++ DuckCode/src/Duck.java
@@ -0,0 +1,27 @@
+public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    public Duck() {
+
+    }
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("All the ducks float :)");
+    }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+}
